00037320b36b7cfe990a3e6f4457c8773cbecc11
MurilloSSJ <murillostore@gmail.com>
2023-12-27 21:56:03 -0300

    feat: create wrapper component

---
+++ src/components/containers/wrapperContainer/styles.ts
@@ -0,0 +1,27 @@
+import { styled } from "@/styles";
+
+export const WrapperContainer = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'start',
+    width: '100%',
+    height: '100%',
+    color: '$white',
+    backgroundColor: '$dark_300',
+    borderRadius: '.25rem',
+    gapColumn: '10px',
+    variants: {
+        padding: {
+            true: {
+                padding: '30px',
+            },
+            false: {
+                padding: '0px',
+            }
+        }
+    },
+    defaultVariants: {
+        padding: true
+    }
+})
