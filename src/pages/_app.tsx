9c803a946f2e624537a33c6c667d2176edf14bd6
MurilloSSJ <murillostore@gmail.com>
2023-12-27 21:59:59 -0300

    feat: add side menu in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Container, Content } from './style'
+import { SideMenu } from '@/components/menu/sideMenu'
+import { useRouter } from 'next/router'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { asPath } = useRouter()
+  const isAuthRoute = asPath === '/' || asPath.startsWith('/auth')
   return (
     <Container>
+      {!isAuthRoute && <SideMenu />}
       <Content>
         <Component {...pageProps} />
       </Content>
